b997c9c9ea51241307b50ff470599add0827464b
Vedang Manerikar <ved.manerikar@gmail.com>
2017-04-22 16:08:36 +0530

    Modify code to use a single reified object
    Since we are reifying the same interface, we might as well do it just
    once, and reuse that object in the various chained stages of the Flink
    pipeline.

--- src/clink/socket_window_wordcount.clj
+++ src/clink/socket_window_wordcount.clj
@@ -5,6 +5,23 @@
            org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
            org.apache.flink.streaming.api.windowing.time.Time))
 
+(defn- get-flinky-word-counter
+  []
+  (reify
+    IWordWithCount
+    (flatMap [this value out]
+      (doseq [word (cs/split value #"\s")]
+        (println "Word: " word)
+        (.collect out
+                  (WordWithCount. word 1))))
+
+    (getKey [this word-with-count]
+      (.word ^WordWithCount word-with-count))
+
+    (reduce [this a b]
+      (WordWithCount. (.word ^WordWithCount a)
+                      (+ (.wcount ^WordWithCount a)
+                         (.wcount ^WordWithCount b))))))
 (defn -main
   [& args]
   (let [execution-env (StreamExecutionEnvironment/getExecutionEnvironment)
@@ -12,25 +29,12 @@
                                 "localhost"
                                 9000
                                 "\n")
+        ^IWordWithCount word-counter-helper (get-flinky-word-counter)
         window-counts (.. text
-                          (flatMap (reify
-                                     IWordWithCount
-                                     (flatMap [this value out]
-                                       (doseq [word (cs/split value #"\s")]
-                                         (println "Word: " word)
-                                         (.collect out
-                                                   (WordWithCount. word 1))))))
-                          (keyBy (reify
-                                   IWordWithCount
-                                   (getKey [this word-with-count]
-                                     (.word ^WordWithCount word-with-count))))
+                          (flatMap word-counter-helper)
+                          (keyBy word-counter-helper)
                           (timeWindow (Time/seconds 5))
-                          (reduce (reify
-                                    IWordWithCount
-                                    (reduce [this a b]
-                                      (WordWithCount. (.word ^WordWithCount a)
-                                                      (+ (.wcount ^WordWithCount a)
-                                                         (.wcount ^WordWithCount b)))))))]
+                          (reduce word-counter-helper))]
     (.. window-counts
         print
         (setParallelism 1))
