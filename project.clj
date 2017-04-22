168ddd0dfabae32f3a391d524b1eacff7a60f08c
Vedang Manerikar <ved.manerikar@gmail.com>
2017-04-22 15:51:20 +0530

    Follow Flink instructions for Lambda support
    Check if this will help with retaining types during compilation. (I
    don't have hopes for this, but trying never hurt anyone)
    
    Information about the changes:
    https://ci.apache.org/projects/flink/flink-docs-release-1.2/dev/java8.html

--- project.clj
+++ project.clj
@@ -7,6 +7,14 @@
                  [org.apache.flink/flink-java "1.2.0"]
                  [org.apache.flink/flink-streaming-java_2.10 "1.2.0"]
                  [org.apache.flink/flink-clients_2.10 "1.2.0"]]
+  :pom-plugins [[org.apache.maven.plugins/maven-compiler-plugin "3.6.1"
+                 {:configuration ([:source "1.8"]
+                                  [:target "1.8"]
+                                  [:compilerId "jdt"])
+                  :dependencies [:dependency
+                                 [:groupId org.eclipse.tycho]
+                                 [:artifactId tycho-compiler-jdt]
+                                 [:version "1.0.0"]]}]]
   :global-vars {*warn-on-reflection* true}
   :aot [clink.socket-window-wordcount]
   :main clink.socket-window-wordcount)
