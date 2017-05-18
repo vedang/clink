bab6c3ac8c500ced8ae3db521310225206267b9e
Vedang Manerikar <ved.manerikar@gmail.com>
2017-05-19 00:30:36 +0530

    Convert the socket-window wordcounter to a defrec
    Allows me to specify the :load-ns option, which is very useful, as
    mentioned in a previous commit message.

--- src/clink/streaming/examples/socket/socket_window_wordcount.clj
+++ src/clink/streaming/examples/socket/socket_window_wordcount.clj
@@ -1,27 +1,31 @@
 (ns clink.streaming.examples.socket.socket-window-wordcount
   (:gen-class)
+  (:refer-clojure :exclude [reduce])
   (:require [clojure.string :as cs])
   (:import [clink.examples.wordcount IWordWithCount WordWithCount]
            org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
            org.apache.flink.streaming.api.windowing.time.Time))
 
-(defn- get-flinky-word-counter
-  []
-  (reify
-    IWordWithCount
-    (flatMap [this value out]
-      (doseq [word (cs/split value #"\s")]
-        (println "Word: " word)
-        (.collect out
-                  (WordWithCount. word 1))))
 
-    (getKey [this word-with-count]
-      (.word ^WordWithCount word-with-count))
+(defrecord FlinkyWordCounter
+    []
+  :load-ns true
+  IWordWithCount
+  (flatMap [this value out]
+    (doseq [word (cs/split value #"\s")]
+      (println "Word: " word)
+      (.collect out
+                (WordWithCount. word 1))))
 
-    (reduce [this a b]
-      (WordWithCount. (.word ^WordWithCount a)
-                      (+ (.wcount ^WordWithCount a)
-                         (.wcount ^WordWithCount b))))))
+  (getKey [this word-with-count]
+    (.word ^WordWithCount word-with-count))
+
+  (reduce [this a b]
+    (WordWithCount. (.word ^WordWithCount a)
+                    (+ (.wcount ^WordWithCount a)
+                       (.wcount ^WordWithCount b)))))
+
+
 (defn -main
   [& args]
   (let [execution-env (StreamExecutionEnvironment/getExecutionEnvironment)
@@ -29,7 +33,7 @@
                                 "localhost"
                                 9000
                                 "\n")
-        ^IWordWithCount word-counter-helper (get-flinky-word-counter)
+        ^IWordWithCount word-counter-helper (FlinkyWordCounter.)
         window-counts (.. text
                           (flatMap word-counter-helper)
                           (keyBy word-counter-helper)
