8475e6cef9316653467865b3054d64247a7659b8
Vedang Manerikar <ved.manerikar@gmail.com>
2017-05-19 00:34:29 +0530

    Update README

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # clink
 
-All the Flink examples, written in Clojure
+All the Flink examples, written in Clojure.
+
+*Note*: This is a work in progress at the moment, so don't go in expecting too much.
 
 ## License
 
-Copyright © 2017 Helpshift
+Copyright © 2017 Vedang Manerikar
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clink "0.1.0-SNAPSHOT"
   :description "All the examples in Flink Documentation, but in Clojure"
-  :url "https://helpshift.com"
+  :url "https://vedang.me/techlog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -8,14 +8,6 @@
                  [org.apache.flink/flink-streaming-java_2.10 "1.2.0"]
                  [org.apache.flink/flink-clients_2.10 "1.2.0"]
                  [org.apache.flink/flink-connector-kafka-0.10_2.10 "1.2.0"]]
-  :pom-plugins [[org.apache.maven.plugins/maven-compiler-plugin "3.6.1"
-                 {:configuration ([:source "1.8"]
-                                  [:target "1.8"]
-                                  [:compilerId "jdt"])
-                  :dependencies [:dependency
-                                 [:groupId org.eclipse.tycho]
-                                 [:artifactId tycho-compiler-jdt]
-                                 [:version "1.0.0"]]}]]
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["src/main/java"]
   :aot [clink.streaming.examples.socket.socket-window-wordcount
